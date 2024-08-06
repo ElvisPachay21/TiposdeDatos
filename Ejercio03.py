40810bf0f1ad627c2468de2ff35f581f251f771b
ElvisPachay <elvis.pachay@tecnologicoliceocristiano.edu.ec>
2024-08-06 14:42:13 -0500

    finalizado

--- Ejercio03.py
+++ Ejercio03.py
@@ -1,8 +1,28 @@
-F1 = input ("Ingrese la fecha con este formato DD, MM AAAA: ")
-D = F1[0:2]
-A = F1 [-4:]
-F2 = F1[4:8] #son 4 letras
-F3 = F1[4:9] #son 5 letras 
-F4 = F1[4:10] #son 6 letras
-F5 = F1[4:11] #son 7 letras
-F6 = F1
+def calcular_media(puntuaciones):
+    return sum(puntuaciones) / len(puntuaciones)
+
+def determinar_califacion(media):
+    if 90 <= media <= 100:
+        return "A"
+    elif 80 <= media < 90:
+        return "B"
+    elif 70 <= media < 80:
+        return "C"
+    elif 60 <= media < 70:
+        return "D"
+    elif 60<= media < 70:
+        return "E"
+    
+puntuaciones = []
+for i in range(4):
+    puntuacion = int(input(f"Ingresa la puntuacion{i+1}: "))
+    puntuaciones.append(puntuacion)
+    
+media = calcular_media(puntuaciones)
+
+calificacion = determinar_califacion(media)
+
+print(f"Las puntuaciones ingresadas son: {puntuaciones}")
+print(f"La media de las puntuaciones es: {media}")
+print(f"La calificacion correspondiente es: {calificacion}")
+                       
